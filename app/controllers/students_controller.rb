a12c5b79546ca8124a6b38980fd735f35efb824a
Nana <72888369+nanaama527@users.noreply.github.com>
2021-05-12 12:53:41 -0400

    commit tree 5be840c4207b5914bbe8d86e0682fce1aaaf9c8c

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,15 +2,32 @@ class StudentsController < ApplicationController
   before_action :set_student, only: :show
   
   def index
-    @students = Student.all
+    @students = students
   end
 
   def show
+    @student = find_student
+  end
+
+  def activate
+    student = find_student
+    student.active = !student.active
+    student.save
+    redirect_to student_path(student)
   end
 
   private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  def students
+    Student.all
+  end
+
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def find_student 
+    Student.find(params[:id])
+  end
 end
